841f7391f6cd540a994c306909ee0597e999df83
rkskek1226 <hkcho01@naver.com>
2022-06-25 20:16:31 +0900

    hand_gesture_model CNN

--- hand_gesture_model_cnn.py
+++ hand_gesture_model_cnn.py
@@ -30,20 +30,20 @@ from tensorflow.keras.applications import ResNet50
 # loss = history.history["loss"]
 
 
-BATCH_SIZE = 20
+BATCH_SIZE = 5
 image_width = 640
 image_height = 480
 
-train = ImageDataGenerator(rescale=1. / 255, rotation_range=20, width_shift_range=0.1,
-                           height_shift_range=0.1, shear_range=0.2, zoom_range=0.3)
-train_generator = train.flow_from_directory("hand_gesture_data/data3/train", target_size=(image_height, image_width),
+train = ImageDataGenerator(rescale=1. / 255, rotation_range=10, width_shift_range=0.1,
+                           height_shift_range=0.1, shear_range=0.1, zoom_range=0.1)
+train_generator = train.flow_from_directory("hand_gesture_data/data3/train/", target_size=(image_width, image_height),
                                             color_mode="rgb", batch_size=BATCH_SIZE, seed=1, shuffle=True,
-                                            class_mode="categorical")
+                                            class_mode="sparse")
 
 test = ImageDataGenerator(rescale=1.0 / 255.0)
-test_generator = test.flow_from_directory("hand_gesture_data/data3/test", target_size=(image_height, image_width),
+test_generator = test.flow_from_directory("hand_gesture_data/data3/test/", target_size=(image_width, image_height),
                                           color_mode="rgb", batch_size=BATCH_SIZE, seed=2, shuffle=True,
-                                          class_mode="categorical")
+                                          class_mode="sparse")
 
 
 model = Sequential()
@@ -57,8 +57,9 @@ model.add(Dense(10, activation="softmax"))
 
 model.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-history = model.fit(train_generator, validation_data=test_generator, epochs=10, verbose=2)
+train_history = model.fit(train_generator, epochs=10, verbose=2)
+test_history = model.evaluate(test_generator)
 
-
+model.save("model_cnn.h5")
 
 
