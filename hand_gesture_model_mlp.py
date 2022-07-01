96edd672810c909a079e7801368269478f3d89de
rkskek1226 <hkcho01@naver.com>
2022-07-01 14:54:51 +0900

    hand_gesture_model_mlp_dropout

--- hand_gesture_model_mlp.py
+++ hand_gesture_model_mlp.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, Activation, BatchNormalization
 from keras.utils import np_utils
 from sklearn.preprocessing import LabelEncoder
 from keras.models import load_model
@@ -24,7 +24,7 @@ np.random.seed(seed)
 tf.random.set_seed(seed)
 
 Data_set = pd.read_csv("hand_gesture_data/hand_gesture_data2.csv")
-x = Data_set.iloc[:, :45].astype(float)
+x = Data_set.iloc[:, 20:45].astype(float)
 y = Data_set.iloc[:, 45]
 
 # e = LabelEncoder()
@@ -35,61 +35,65 @@ y = np_utils.to_categorical(y)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, stratify=y, random_state=1)
 
+
 model = Sequential()
-model.add(Dense(64, input_dim=45, activation="relu"))
-model.add(Dense(48, activation="relu"))
-model.add(Dense(24, activation="relu"))
-model.add(Dense(10, activation="softmax"))
+
+
+# model.add(Dense(64, input_dim=25, activation="relu"))
+# model.add(Dense(128, activation="relu"))
+# model.add(Dropout(0.25))
+# model.add(Dense(256, activation="relu"))
+# model.add(Dense(128, activation="relu"))
+# model.add(Dense(64, activation="relu"))
+# model.add(Dropout(0.25))
+# model.add(Dense(32, activation="relu"))
+# model.add(Dense(10, activation="softmax"))
+
+
+model.add(Dense(64, input_dim=25))
+model.add(Activation("relu"))
+model.add(Dropout(0.25))
+model.add(Dense(128))
+model.add(Activation("relu"))
+model.add(Dropout(0.25))
+model.add(Dense(256))
+model.add(Activation("relu"))
+model.add(Dropout(0.25))
+model.add(Dense(128))
+model.add(Activation("relu"))
+model.add(Dropout(0.25))
+model.add(Dense(64))
+model.add(Activation("relu"))
+model.add(Dropout(0.25))
+model.add(Dense(32))
+model.add(Activation("relu"))
+model.add(Dropout(0.25))
+model.add(Dense(10))
+model.add(Activation("softmax"))
+
+
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-history = model.fit(x_train, y_train, epochs=10, batch_size=10, validation_split=0.2)
+history = model.fit(x_train, y_train, epochs=60, batch_size=10, validation_split=0.2)
 print("Accuracy : {}".format(model.evaluate(x_test, y_test)[1]))
 
 
 plt.subplot(211)
 plt.plot(history.history["accuracy"])
 plt.plot(history.history["val_accuracy"])
-plt.title("accuracy")
+plt.title("Accuracy")
 plt.xlabel("epoch")
 plt.ylabel("accuracy")
-plt.legend(["train", "validation"], loc="upper left")
+plt.legend(["train", "validation"], loc="lower right")
 
 plt.subplot(212)
 plt.plot(history.history["loss"])
 plt.plot(history.history["val_loss"])
-plt.title("loss")
+plt.title("Loss")
 plt.xlabel("epoch")
 plt.ylabel("loss")
 plt.legend(["train", "validation"], loc="upper right")
 plt.tight_layout()
 plt.show()
-
-
-
-# CNN
-#
-# (x_train, y_train), (x_test, y_test) = mnist.load_data()
-# x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype("float32") / 255
-# x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype("float32") / 255
-# y_train = np_utils.to_categorical(y_train)
-# y_test = np_utils.to_categorical(y_test)
-#
-# model = Sequential()
-# model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(28, 28, 1), activation="relu"))
-# model.add(Conv2D(64, (3, 3), activation="relu"))
-# model.add(MaxPooling2D(pool_size=2))
-# model.add(Dropout(0.25))
-# model.add(Flatten())
-# model.add(Dense(128, activation="relu"))
-# model.add(Dropout(0.25))
-# model.add(Dense(10, activation="softmax"))
-#
-# model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-
-
-
-
-
-
 
 
 # model.save("model_test.h5")
